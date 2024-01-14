2207c94c45f3753aa0441de1108ca3166e443e0a
Colin Woodbury <colin@fosskers.ca>
2024-01-14 19:00:06 +0900

    feat: exploring simple functions
    As well as the introduction of a condition for when a file is expected
    but a directory is given.

--- src/filepaths.lisp
+++ src/filepaths.lisp
@@ -16,10 +16,22 @@
   "Is the final component of a PATH some given CHILD?")
 
 (defun absolutep (path)
-  "Is the given PATH a full, absolute path?")
+  "Is the given PATH a full, absolute path?"
+  (uiop:absolute-pathname-p path))
+
+#+nil
+(absolutep #p"/home/colin/foo.txt")
+#+nil
+(absolutep #p"foo.txt")
 
 (defun relativep (path)
-  "Is the given PATH a relative one?")
+  "Is the given PATH a relative one?"
+  (uiop:relative-pathname-p path))
+
+#+nil
+(relativep #p"/home/colin/foo.txt")
+#+nil
+(relativep #p"foo.txt")
 
 (defun directoryp (path)
   "Does the given PATH exist on the file system and point to a directory?")
@@ -27,14 +39,50 @@
 (defun filep (path)
   "Does the given PATH exist on the file system and point to a normal file?")
 
+(declaim (ftype (function ((or pathname string)) simple-string) base))
 (defun base (path)
-  "The non-extension, non-directory portion of the filename of a PATH.")
+  "The non-extension, non-directory portion of the filename of a PATH."
+  (let ((b (pathname-name path)))
+    (if (not b)
+        (error 'no-filename :path path)
+        b)))
+
+#+nil
+(base #p"/foo/bar/baz.txt")
+#+nil
+(base "/foo/bar/baz.txt")
+#+nil
+(base #p"/foo/bar/ゆびわ.txt")
+#+nil
+(base "/foo/bar/")
+#+nil
+(base 7)
 
 (defun with-base (path new)
   "Swap the base portion of a PATH with a NEW one.")
 
+(declaim (ftype (function ((or pathname string)) simple-string) name))
 (defun name (path)
-  "The filename of a PATH with no other directory components.")
+  "The filename of a PATH with no other directory components."
+  (let ((n (file-namestring path)))
+    (if (= 0 (length n))
+        (error 'no-filename :path path)
+        n)))
+
+#+nil
+(name #p"baz.txt")
+#+nil
+(name #p"/foo/bar/baz.txt")
+#+nil
+(name "/foo/bar/baz.txt")
+#+nil
+(name #p"/foo/bar/ゆびわ.txt")
+#+nil
+(name #p"/foo/bar/")
+#+nil
+(name "")
+#+nil
+(name 7)
 
 (defun with-name (path new)
   "Swap the filename portion of a PATH with a NEW one.")
@@ -68,3 +116,13 @@
 
 (defun from-string (s)
   "Convert a string into a project filepath object.")
+
+;; --- Conditions --- ;;
+
+(define-condition no-filename (error)
+  ((path :initarg :path :reader no-filename-path))
+  (:documentation "The given path was expected to be a filename, but it may have been a directory
+or empty string instead.")
+  (:report (lambda (condition stream)
+             (format stream "The given path was expected to be a filename: ~a"
+                     (no-filename-path condition)))))
